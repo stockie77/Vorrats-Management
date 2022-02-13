4077aea003cab3c41ebc674757106d4ac2957391
stockie77 <60014960+stockie77@users.noreply.github.com>
2022-02-13 11:58:11 +0100

    BarcodeFunction

---
+++ app/src/main/java/com/example/vorratsMngt/MainActivity.kt
@@ -0,0 +1,163 @@
+package com.example.vorratsMngt
+
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.SurfaceHolder
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.vorratsMngt.databinding.ActivityMainBinding
+
+import java.io.IOException
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import com.google.android.gms.vision.Detector.Detections
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var B: ActivityMainBinding
+    private val requestCodeCameraPermission = 1001
+    private lateinit var cameraSource: CameraSource
+    private lateinit var barcodeDetector: BarcodeDetector
+    private var scannedValue = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        B = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(B.root)
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity, android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            askForCameraPermission()
+        } else {
+            setupControls()
+        }
+        B.buScan.setOnClickListener{
+            val aniSlide: Animation =
+                AnimationUtils.loadAnimation(this@MainActivity, R.anim.scanner_animation)
+            B.barcodeLine.startAnimation(aniSlide)
+            Log.d("Start","app")
+
+        }
+
+    }
+
+
+    private fun setupControls() {
+        barcodeDetector =
+            BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.ALL_FORMATS).build()
+
+        cameraSource = CameraSource.Builder(this, barcodeDetector)
+            .setRequestedPreviewSize(1920, 1080)
+            .setAutoFocusEnabled(true) //you should add this feature
+            .build()
+
+        B.cameraSurfaceView.getHolder().addCallback(object : SurfaceHolder.Callback {
+            @SuppressLint("MissingPermission")
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                try {
+                    //Start preview after 1s delay
+                    cameraSource.start(holder)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+
+            @SuppressLint("MissingPermission")
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+                try {
+                    cameraSource.start(holder)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                cameraSource.stop()
+            }
+        })
+
+
+        barcodeDetector.setProcessor(object : Detector.Processor<Barcode> {
+            override fun release() {
+                Toast.makeText(applicationContext, "Scanner has been closed", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            override fun receiveDetections(detections: Detections<Barcode>) {
+                val barcodes = detections.detectedItems
+                if (barcodes.size() == 1) {
+                    scannedValue = barcodes.valueAt(0).rawValue
+                    Log.d("TEST $scannedValue","Ausgabe")
+                    B.tvResult.setText(scannedValue)
+                    //Don't forget to add this line printing value or finishing activity must run on main thread
+                    /*
+                    runOnUiThread {
+
+                        cameraSource.stop()
+                        //Toast.makeText(this@MainActivity, "value- $scannedValue", Toast.LENGTH_SHORT).show()
+                        B.tvResult.text="value- $scannedValue"
+                        Log.d("TEST $scannedValue","Ausgabe")
+
+                        //finish()
+                    }  */
+                }else
+                {
+
+                   // Toast.makeText(this@MainActivity, "value- else", Toast.LENGTH_SHORT).show()
+                  //  B.tvResult.text="value- else"
+                }
+            }
+        })
+    }
+
+    private fun askForCameraPermission() {
+        ActivityCompat.requestPermissions(
+            this@MainActivity,
+            arrayOf(android.Manifest.permission.CAMERA),
+            requestCodeCameraPermission
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == requestCodeCameraPermission && grantResults.isNotEmpty()) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setupControls()
+            } else {
+                Toast.makeText(applicationContext, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraSource.stop()
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
+    }
+
+
+}
+
+
+
+
